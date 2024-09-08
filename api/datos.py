62347e33036fe55a8eedbf6ad94831cf51b62fbf
DaMoH <83809053+DanielMoren0@users.noreply.github.com>
2024-09-08 13:39:00 -0500

    commit funcionando

---
+++ api/datos.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from sodapy import Socrata
+
+
+def obtener_datos(limite, departamento):
+    cliente = Socrata("www.datos.gov.co", None)
+    resultados = cliente.get("ch4u-f3i5", limit=limite, departamento=departamento)
+    return pd.DataFrame.from_records(resultados)
